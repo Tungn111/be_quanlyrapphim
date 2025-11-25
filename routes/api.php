105a1ff5b2d1bdc401c5c8f20fbc5844fc66d5ed
son <trinhminhson2004@gmail.com>
2025-11-25 18:15:46 +0700

    update BE Phim

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PhongChieuController;
+use App\Http\Controllers\PhimController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,3 +13,11 @@ use Illuminate\Support\Facades\Route;
     Route::post('/phong-chieu/change-status', [PhongChieuController::class, 'changeStatus']);
     Route::post('/phong-chieu/tao-ghe-auto', [PhongChieuController::class, 'taoGheAuto']);
     Route::post('/phong-chieu/tim-kiem', [PhongChieuController::class, 'search']);
+
+
+    Route::get('/phim/get-data', [PhimController::class, 'getData']);
+    Route::post('/phim/add-data', [PhimController::class, 'addData']);
+    Route::post('/phim/update', [PhimController::class, 'update']);
+    Route::post('/phim/delete', [PhimController::class, 'destroy']);
+    Route::post('/phim/change-status', [PhimController::class, 'changeStatus']);
+    Route::post('/phim/tim-kiem', [PhimController::class, 'search']);
